d2deb7f69806b2a12787cce8a4b3af28008c0b26
민주 <hinote444@gmail.com>
2023-09-29 21:45:59 +0900

    feat: 컴퓨터의 랜덤 번호를 리턴해주는 기능 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -8,10 +8,14 @@ import java.util.List;
 public class Computer {
     private List<Integer> computerGameNumbers;
 
+    public List<Integer> getComputerGameNumbers() {
+        return computerGameNumbers;
+    }
+
     public void setupGameNumber() {
         computerGameNumbers = new ArrayList<>();
         while (computerGameNumbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1,9);
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerGameNumbers.contains(randomNumber)) {
                 computerGameNumbers.add(randomNumber);
             }
