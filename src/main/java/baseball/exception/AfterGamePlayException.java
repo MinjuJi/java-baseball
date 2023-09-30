d0aafc750c8de2228fd6c1ebe3df141262e4bdbd
민주 <hinote444@gmail.com>
2023-09-30 21:16:00 +0900

    feat: 게임 종료 후 다음 게임을 위해 입력 받은 숫자의 범위가 적절한지 확인해주는 기능 구현

---
+++ src/main/java/baseball/exception/AfterGamePlayException.java
@@ -0,0 +1,11 @@
+package baseball.exception;
+
+public class AfterGamePlayException {
+    private static final String PERMIT_NUMBER_REGEX = "[1-2]+";
+    private static final int PERMIT_NUMBER_LENGTH = 1;
+
+    public void isRightNumber(String numberOfString) {
+        if (!numberOfString.matches(PERMIT_NUMBER_REGEX))
+            throw new IllegalArgumentException("1~2까지의 숫자만 입력해야 합니다.");
+    }
+}
